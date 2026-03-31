9a6fafb03459a9063c22bc577e652ddbd37f052e
moynul2k3 <moynul.officials@gmail.com>
2026-03-31 11:32:50 +0600

    task modify

--- tasks/notify.py
+++ tasks/notify.py
@@ -1,20 +1,18 @@
 from app.utils.task_decorators import every
 from app.utils.send_email import send_email
-
-@every(seconds=30)
-def check_every_schedule():
-    print("wake up Call")
-
+import pytz
+from app.config import settings
+dhaka_tz = pytz.timezone('Asia/Dhaka')
 
 
-@every(hour=8, minute=0)
+@every(day=1, hour=8, minute=0, tz=dhaka_tz)
 async def check_email_schedule():
     await send_email(
         subject="Good Morning Message",
         to=["softvence.moynul@gmail.com"],
-        html_message="<h3>Hello Moynul! </h3><p>Good Morning. How are you.</p>",
-        from_name="My App",
-        from_email='moynul.officials@gmail.com'
+        html_message="<h3>Hello Moynul! </h3><p>Good Morning. How are you. Don't worry, I am running.</p>",
+        from_name=settings.APP_NAME,
+        from_email=settings.DEFAULT_FROM_EMAIL
     )
     print("send Good Morning email everyday at 8:00 AM")
     
