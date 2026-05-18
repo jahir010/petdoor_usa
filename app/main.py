790604d80f0954ae2e358a931dc67045a8ab5da6
moynul2k3 <moynul.officials@gmail.com>
2026-05-18 08:52:10 +0600

    ...

--- app/main.py
+++ app/main.py
@@ -70,7 +70,7 @@ ALLOWED_HOST = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
-    "https://petdoorusa.netlify.app",
+    "https://petdoorusa.netlify.app"
 ] 
 app.add_middleware(
     CORSMiddleware,
